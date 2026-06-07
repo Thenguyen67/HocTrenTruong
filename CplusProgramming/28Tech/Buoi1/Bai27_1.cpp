509c5ba79a20e560f9179c96ee3ce58918efea8f
Theanuen <itachim905@gmail.com>
2026-06-07 14:11:02 +0700

    Delete CplusProgramming directory

--- CplusProgramming/28Tech/Buoi1/Bai27_1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std ;
-
-int main()
-{
-    unsigned int Day ;
-    cin >> Day ;
-    cout << int(Day / 365) << " " << int((Day % 365) / 7) << " " << int((Day % 365) % 7) ;
- }
